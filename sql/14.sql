d2482b4586df711e59e6d4551d78a99c58ddde30
rutisoniaisimbi4 <isimbi.rutisonia@gmail.com>
2026-03-11 20:38:04 -0700

    completed pagila hw2 SQL queries

--- sql/14.sql
+++ sql/14.sql
@@ -1,10 +1,12 @@
 SELECT
-    EXTRACT(YEAR FROM payment_date) AS "Year",
-    EXTRACT(MONTH FROM payment_date) AS "Month",
-    SUM(amount) AS "Total Revenue"
-FROM payment
+    EXTRACT(YEAR FROM rental.rental_date)::int AS "Year",
+    EXTRACT(MONTH FROM rental.rental_date)::int AS "Month",
+    SUM(payment.amount) AS "Total Revenue"
+FROM rental
+JOIN payment
+    ON payment.rental_id = rental.rental_id
 GROUP BY ROLLUP(
-    EXTRACT(YEAR FROM payment_date),
-    EXTRACT(MONTH FROM payment_date)
+    EXTRACT(YEAR FROM rental.rental_date),
+    EXTRACT(MONTH FROM rental.rental_date)
 )
 ORDER BY 1 NULLS LAST, 2 NULLS LAST;
